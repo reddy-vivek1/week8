6058d7e6f477e9b2a7422b3c5a5871a062cd0855
reddy-vivek1 <eegavivekreddy@gmail.com>
2024-03-06 19:35:23 -0500

    Added delegate but got error in testcases

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack<E> extends ArrayList<E> {
+    private ArrayList<E> delegate;
+
+    public MyStack() {
+        this.delegate = delegate;
+    }
 
     public void push(E e) {
-        add(e);
+        delegate.add(e);
     }
 
     public E pop() {
         if (isEmpty()) throw new EmptyStackException();
-        E e = get(size() - 1);
-        remove(size() -1 );
+        E e = delegate.get(size() - 1);
+        delegate.remove(size() -1 );
         return e;
     }
 
